bcd196b01ab232e3187cf60eb630cb8710f541d7
Jackey Liu <ultrapowerpie@github.com>
2018-03-05 23:16:34 -0500

    added headers

--- search.py
+++ search.py
@@ -26,7 +26,11 @@ if __name__ == '__main__':
         articles, words = counts_map[date]
         counts_map[date] = [articles + 1, words + a[1]]
 
+    headers = ['Date', 'Articles', 'Counts']
+
     counts = [','.join([d, str(v[0]), str(v[1])]) for d, v in counts_map.items()]
+
+    counts = [headers] + counts
 
     with open(sys.argv[2], 'w') as f:
         f.write('\n'.join(counts))
